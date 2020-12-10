c868b71747f01ab32f73e603baf7244a23fd95b3
chimbox <chimboxsoftgve@gmail.com>
2020-12-10 10:00:08 -0700

    Se arreglaron detalles, listo para revisar.

--- Patolli_Filters/src/filters/FilterVictoria.java
+++ Patolli_Filters/src/filters/FilterVictoria.java
@@ -1,12 +1,17 @@
 package filters;
 
+import dominio.Casilla;
 import dominio.EstadoPartida;
+import dominio.Ficha;
 import dominio.Jugador;
 import dominio.Partida;
+import dominio.Tablero;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
  * Clase encargada de ajustar el estado de la Partida para definir a un ganador.
+ *
  * @author alfonsofelix
  */
 public class FilterVictoria extends Filter<Partida, Partida> {
@@ -18,9 +23,24 @@ public class FilterVictoria extends Filter<Partida, Partida> {
         if (partida.getEstado() == EstadoPartida.INICIADA) {
 
             for (Jugador jug : partida.getJugadores()) {
-                if (jug.getNumFrijoles() <= 0 && partida.getTurnos().contains(jug)) {
+                if ((jug.getNumFrijoles() <= 0 || (jug.getFichas().isEmpty() && jug.getFichasGanadoras().isEmpty())) && partida.getTurnos().contains(jug)) {
+                    Tablero tablero=partida.getTablero();
+                    LinkedList<Casilla> casillas = tablero.getCasillas();
                     Queue<Jugador> turnos = partida.getTurnos();
                     turnos.remove(jug);
+                    for (Casilla casilla : casillas) {
+                        if (casilla.getFicha() != null) {
+                            for (Ficha ficha : jug.getFichas()) {
+
+                                if (casilla.getFicha().equals(ficha)) {
+                                    casilla.setFicha(null);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    tablero.setCasillas(casillas);
+                    partida.setTablero(tablero);
                     partida.setTurnos(turnos);
                 }
 
@@ -36,20 +56,20 @@ public class FilterVictoria extends Filter<Partida, Partida> {
                 }
 
             }
-            if(partida.getTurnos().size() == 1){
-                Jugador ganador=partida.getTurnos().poll();
+            if (partida.getTurnos().size() == 1) {
+                Jugador ganador = partida.getTurnos().poll();
                 partida.setGanador(ganador);
                 partida.setEstado(EstadoPartida.TERMINADA);
             }
             if (partida.getTurnos().isEmpty()) {
                 Jugador ganador = null;
                 for (Jugador jugador : partida.getJugadores()) {
-                    if(!jugador.perdio()){
-                        ganador=jugador;
+                    if (!jugador.perdio()) {
+                        ganador = jugador;
                         break;
                     }
                 }
-                
+
                 for (Jugador jugador : partida.getJugadores()) {
                     if (jugador.getFichasGanadoras().size() > ganador.getFichasGanadoras().size() && !ganador.perdio()) {
                         ganador = jugador;
@@ -60,7 +80,7 @@ public class FilterVictoria extends Filter<Partida, Partida> {
             }
         }
         partida.setSaliendo(false);
-        
+
         output.put(partida);
         output.doChain();
     }
